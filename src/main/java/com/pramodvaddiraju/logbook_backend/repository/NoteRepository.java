c4c3ce4b6f18dfc26a7ecdfdfdb1138efd697385
pramodvaddi <pramodvaddiraju@gmail.com>
2026-02-10 19:24:13 +0530

    added method in repository to search by title

--- src/main/java/com/pramodvaddiraju/logbook_backend/repository/NoteRepository.java
+++ src/main/java/com/pramodvaddiraju/logbook_backend/repository/NoteRepository.java
@@ -3,8 +3,10 @@ package com.pramodvaddiraju.logbook_backend.repository;
 import com.pramodvaddiraju.logbook_backend.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
-
+        Optional<Note> findByTitleIgnoreCase(String titleName);
 
 }
